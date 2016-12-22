34a399720f65bdc446e96ab66f9aba2b5ba245f9
SYFZzzz <mac@macdeMacBook-Air.local>
2016-12-22 09:51:44 +0800

    commit
    alter

--- BaseTabBarController.m
+++ BaseTabBarController.m
@@ -43,13 +43,7 @@
     Mine.tabBarItem.image = [UIImage imageNamed:@"Image-3"];
     UINavigationController *navmine = [[UINavigationController alloc] initWithRootViewController:Mine];
     
-    //为tabBarController添加子控制器
-    informationViewController *infomation = [[informationViewController alloc] init];
-    //设置子控制器的图标等
-    infomation.tabBarItem.title = @"消息";
-    infomation.tabBarItem.image = [UIImage imageNamed:@"Image"];
-    UINavigationController * navinfoma = [[UINavigationController alloc] initWithRootViewController:infomation];
-    self.viewControllers=@[navhome,navnear,navinfoma,navmine];
+    self.viewControllers=@[navhome,navnear,navmine];
     
     
     
